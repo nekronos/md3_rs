a84aa61e130822bcc087890dc8c8d74ed9a4e1e2
nekronos <lende.vegard@gmail.com>
2016-12-10 17:36:34 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,316 @@
+
+extern crate byteorder;
+
+use byteorder::{ReadBytesExt, LittleEndian};
+use std::io::{Cursor};
+
+const MD3_MAGIC: i32 = 0x33504449;
+const MAX_QPATH: usize = 64;
+
+#[derive(Debug)]
+struct Vec3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+#[derive(Debug)]
+pub struct Md3 {
+    header: Md3Header,
+    frames: Vec<Frame>,
+    tags: Vec<Tag>,
+    surfaces: Vec<Surface>,
+}
+
+#[derive(Debug)]
+struct Md3Header {
+    ident: i32,
+    version: i32,
+    name: String,
+    flags: i32,
+    num_frames: i32,
+    num_tags: i32,
+    num_surfaces: i32,
+    num_skins: i32,
+    ofs_frames: i32,
+    ofs_tags: i32,
+    ofs_surfaces: i32,
+    ofs_eof: i32,
+}
+
+#[derive(Debug)]
+struct Frame {
+    min_bounds: Vec3,
+    max_bounds: Vec3,
+    local_origin: Vec3,
+    radius: f32,
+    name: String,
+}
+
+#[derive(Debug)]
+struct Tag {
+    name: String,
+    origin: Vec3,
+    axis: [Vec3; 3],
+}
+
+#[derive(Debug)]
+struct SurfaceHeader {
+    ident: i32,
+    name: String,
+    flags: i32,
+    num_frames: i32,
+    num_shaders: i32,
+    num_verts: i32,
+    num_triangles: i32,
+    ofs_triangles: i32,
+    ofs_shaders: i32,
+    ofs_st: i32,
+    ofs_xyznormal: i32,
+    ofs_end: i32,
+}
+
+#[derive(Debug)]
+struct Shader {
+    name: String,
+    shader_index: i32,
+}
+
+#[derive(Debug)]
+struct Surface {
+    header: SurfaceHeader,
+    shaders: Vec<Shader>,
+    triangles: Vec<Triangle>,
+    tex_coords: Vec<TexCoord>,
+    verticies: Vec<Vec<Vertex>>,
+}
+
+#[derive(Debug)]
+struct Triangle {
+    indexes: [i32; 3],
+}
+
+#[derive(Debug)]
+struct TexCoord {
+    st: [f32; 2],
+}
+
+#[derive(Debug)]
+struct Vertex {
+    x: i16,
+    y: i16,
+    z: i16,
+    normal: i16,
+}
+
+impl Md3 {
+    pub fn from_bytes(bytes: &[u8]) -> Result<Md3, String> {
+        let mut buff = Cursor::new(bytes);
+
+        if Md3::read_s32(&mut buff) == MD3_MAGIC {
+
+            buff.set_position(0);
+            let md3_header = Md3::read_md3_header(&mut buff);
+
+            buff.set_position(md3_header.ofs_frames as u64);
+            let frames = Md3::read_many(&mut buff, Md3::read_frame, md3_header.num_frames as usize);
+
+            buff.set_position(md3_header.ofs_tags as u64);
+            let tags = Md3::read_many(&mut buff, Md3::read_tag, md3_header.num_tags as usize);
+
+            buff.set_position(md3_header.ofs_surfaces as u64);
+            let surfaces = Md3::read_many(&mut buff,
+                                          Md3::read_surface,
+                                          md3_header.num_surfaces as usize);
+
+            let md3 = Md3 {
+                header: md3_header,
+                frames: frames,
+                tags: tags,
+                surfaces: surfaces,
+            };
+
+            Ok(md3)
+        } else {
+            Err(String::from("MD3 magic not matching"))
+        }
+    }
+
+    fn read_many<T, F>(buff: &mut Cursor<&[u8]>, reader: F, count: usize) -> Vec<T>
+        where F: Fn(&mut Cursor<&[u8]>) -> T
+    {
+        let mut vec = Vec::new();
+        for _ in 0..count {
+            vec.push(reader(buff))
+        }
+        vec
+    }
+
+    fn read_s32(buff: &mut Cursor<&[u8]>) -> i32 {
+        buff.read_i32::<LittleEndian>().unwrap()
+    }
+
+    fn read_s16(buff: &mut Cursor<&[u8]>) -> i16 {
+        buff.read_i16::<LittleEndian>().unwrap()
+    }
+
+    fn read_f32(buff: &mut Cursor<&[u8]>) -> f32 {
+        buff.read_f32::<LittleEndian>().unwrap()
+    }
+
+    fn read_vec3(buff: &mut Cursor<&[u8]>) -> Vec3 {
+        Vec3 {
+            x: Md3::read_f32(buff),
+            y: Md3::read_f32(buff),
+            z: Md3::read_f32(buff),
+        }
+    }
+
+    fn read_string(buff: &mut Cursor<&[u8]>, len: usize) -> String {
+        let mut bytes = Vec::new();
+        for _ in 0..len {
+            bytes.push(buff.read_u8().unwrap())
+        }
+        String::from_utf8(bytes.into_iter().take_while(|x| *x != '\0' as u8).collect()).unwrap()
+    }
+
+    fn read_md3_header(buff: &mut Cursor<&[u8]>) -> Md3Header {
+        Md3Header {
+            ident: Md3::read_s32(buff),
+            version: Md3::read_s32(buff),
+            name: Md3::read_string(buff, MAX_QPATH),
+            flags: Md3::read_s32(buff),
+            num_frames: Md3::read_s32(buff),
+            num_tags: Md3::read_s32(buff),
+            num_surfaces: Md3::read_s32(buff),
+            num_skins: Md3::read_s32(buff),
+            ofs_frames: Md3::read_s32(buff),
+            ofs_tags: Md3::read_s32(buff),
+            ofs_surfaces: Md3::read_s32(buff),
+            ofs_eof: Md3::read_s32(buff),
+        }
+    }
+
+    fn read_frame(buff: &mut Cursor<&[u8]>) -> Frame {
+        Frame {
+            min_bounds: Md3::read_vec3(buff),
+            max_bounds: Md3::read_vec3(buff),
+            local_origin: Md3::read_vec3(buff),
+            radius: Md3::read_f32(buff),
+            name: Md3::read_string(buff, 16),
+        }
+    }
+
+    fn read_tag(buff: &mut Cursor<&[u8]>) -> Tag {
+        Tag {
+            name: Md3::read_string(buff, MAX_QPATH),
+            origin: Md3::read_vec3(buff),
+            axis: [Md3::read_vec3(buff), Md3::read_vec3(buff), Md3::read_vec3(buff)],
+        }
+    }
+
+    fn read_surface(buff: &mut Cursor<&[u8]>) -> Surface {
+        let surface_start = buff.position();
+        let surface_header = Md3::read_surface_header(buff);
+
+        buff.set_position(surface_start + surface_header.ofs_shaders as u64);
+        let shaders = Md3::read_many(buff, Md3::read_shader, surface_header.num_shaders as usize);
+
+        buff.set_position(surface_start + surface_header.ofs_triangles as u64);
+        let triangles = Md3::read_many(buff,
+                                       Md3::read_triangle,
+                                       surface_header.num_triangles as usize);
+
+        buff.set_position(surface_start + surface_header.ofs_st as u64);
+        let tex_coords =
+            Md3::read_many(buff, Md3::read_tex_coord, surface_header.num_verts as usize);
+
+        buff.set_position(surface_start + surface_header.ofs_xyznormal as u64);
+        let verticies = Md3::read_many(buff,
+                                       |x| {
+                                           Md3::read_many(x,
+                                                          Md3::read_vertex,
+                                                          surface_header.num_verts as usize)
+                                       },
+                                       surface_header.num_frames as usize);
+
+        Surface {
+            header: surface_header,
+            shaders: shaders,
+            triangles: triangles,
+            tex_coords: tex_coords,
+            verticies: verticies,
+        }
+    }
+
+    fn read_surface_header(buff: &mut Cursor<&[u8]>) -> SurfaceHeader {
+        SurfaceHeader {
+            ident: Md3::read_s32(buff),
+            name: Md3::read_string(buff, MAX_QPATH),
+            flags: Md3::read_s32(buff),
+            num_frames: Md3::read_s32(buff),
+            num_shaders: Md3::read_s32(buff),
+            num_verts: Md3::read_s32(buff),
+            num_triangles: Md3::read_s32(buff),
+            ofs_triangles: Md3::read_s32(buff),
+            ofs_shaders: Md3::read_s32(buff),
+            ofs_st: Md3::read_s32(buff),
+            ofs_xyznormal: Md3::read_s32(buff),
+            ofs_end: Md3::read_s32(buff),
+        }
+    }
+
+    fn read_shader(buff: &mut Cursor<&[u8]>) -> Shader {
+        Shader {
+            name: Md3::read_string(buff, MAX_QPATH),
+            shader_index: Md3::read_s32(buff),
+        }
+    }
+
+    fn read_triangle(buff: &mut Cursor<&[u8]>) -> Triangle {
+        Triangle { indexes: [Md3::read_s32(buff), Md3::read_s32(buff), Md3::read_s32(buff)] }
+    }
+
+    fn read_tex_coord(buff: &mut Cursor<&[u8]>) -> TexCoord {
+        TexCoord { st: [Md3::read_f32(buff), Md3::read_f32(buff)] }
+    }
+
+    fn read_vertex(buff: &mut Cursor<&[u8]>) -> Vertex {
+        Vertex {
+            x: Md3::read_s16(buff),
+            y: Md3::read_s16(buff),
+            z: Md3::read_s16(buff),
+            normal: Md3::read_s16(buff),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn load_rocketl() {
+        let bytes = include_bytes!("../rocketl.md3");
+        let md3 = Md3::from_bytes(bytes).unwrap();
+    }
+
+    #[test]
+    fn load_head() {
+        let bytes = include_bytes!("../head.md3");
+        let md3 = Md3::from_bytes(bytes).unwrap();
+    }
+
+    #[test]
+    fn load_upper() {
+        let bytes = include_bytes!("../upper.md3");
+        let md3 = Md3::from_bytes(bytes).unwrap();
+    }
+
+    #[test]
+    fn load_lower() {
+        let bytes = include_bytes!("../lower.md3");
+        let md3 = Md3::from_bytes(bytes).unwrap();
+    }
+}
